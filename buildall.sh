f0db092c4ab605a1c7e190ae3fd25f42ba3d7cdd
Nick <nick@nimo.uk>
2021-08-22 16:34:39 +0100

    Added buildall script and updated dev build to use it

---
+++ buildall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm umakefile
+for f in umakefile*
+do
+	echo "Processing $f..."
+	ln -s $f umakefile
+	umake clean
+	umake
+	make
+	if [ $? -eq 0 ]; then
+    	echo Build of $f passed
+		rm umakefile
+	else
+    	echo build of $f failed
+		rm umakefile
+		exit 1
+	fi
+done
