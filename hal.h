043271ca526046d62b077b5ed0022840a7b89b0c
Nick <nick@nimo.uk>
2021-08-22 17:16:58 +0100

    Fixed induced error

--- hal.h
+++ hal.h
@@ -1,7 +1,7 @@
 #if defined(__NUVO_M032K)
 #define UART_0_USE_PF2_3
 /* UI defn's */
-#define BL_LED_PORT GPIO_PORT
+#define BL_LED_PORT GPIO_PORTC
 #define BL_LED_PIN 14
 #define BL_SW_PORT GPIO_PORTB
 #define BL_SW_PIN 14
